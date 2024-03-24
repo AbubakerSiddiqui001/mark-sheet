a84485ad5cfee92b177a4e8b0cfd8eaeec7bfad5
Abubaker Siddiqui <162192538+AbubakerSiddiqui001@users.noreply.github.com>
2024-03-25 03:30:21 +0500

    created Mark-sheet

---
+++ main.js
@@ -0,0 +1,16 @@
+var percentage = 35;
+if (percentage >= 80 && percentage < 100) {
+    console.log("A+ Grade");
+}
+else if (percentage >= 70 && percentage <= 79.99) {
+    console.log("A Garde");
+}
+else if (percentage >= 60 && percentage <= 69.99) {
+    console.log("B Grade");
+}
+else if (percentage >= 50 && percentage <= 59.99) {
+    console.log("C Grade");
+}
+else {
+    console.log("Fail");
+}
